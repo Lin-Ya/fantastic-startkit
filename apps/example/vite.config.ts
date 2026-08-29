f6c8c17287c5fa3e317979a3e353df894cc7751f
Hooray <304327508@qq.com>
2026-08-30 01:34:31 +0800

    修复应用运行时警告

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import process from 'node:process'
 import { defineConfig, loadEnv } from 'vite'
 import { parseLoadedEnv } from 'vite-plugin-env-parse'
-import createVitePlugins from './vite/plugins'
+import createVitePlugins from './vite/plugins.ts'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
@@ -41,8 +41,8 @@ export default defineConfig(({ mode, command }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src'),
-        '#': path.resolve(__dirname, 'src/types'),
+        '@': path.resolve(import.meta.dirname, 'src'),
+        '#': path.resolve(import.meta.dirname, 'src/types'),
       },
     },
     css: {
